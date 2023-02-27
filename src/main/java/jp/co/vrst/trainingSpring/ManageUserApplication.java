2a7cae834a915f407b73e86db5fa7fd393857d6c
shuyasuzuki <s.suzuki@vrst.co.jp>
2023-02-27 19:43:51 +0900

    プロジェクト立ち上げ

---
+++ src/main/java/jp/co/vrst/trainingSpring/ManageUserApplication.java
@@ -0,0 +1,13 @@
+package jp.co.vrst.trainingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/vrst/trainingSpring/ManageUserApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.vrst.trainingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
